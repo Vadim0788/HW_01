7ef53e07ed8eceebd79be3a596ec45c9925fba08
Vadim <Haspiev@gmail.com>
2024-11-01 18:33:42 +0500

    Initial commit

---
+++ src/main/java/haspiev/dev/hw_01/Hw01Application.java
@@ -0,0 +1,13 @@
+package haspiev.dev.hw_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hw01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hw01Application.class, args);
+	}
+
+}
